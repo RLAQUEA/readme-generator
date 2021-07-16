83076e7d48c7a5149567efb364a67d5879e303f4
rlaquea <rlaquea14@gmail.com>
2021-07-16 17:32:27 -0400

    fixed makereadme functionality and cleaned up code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-//Required modules
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 // const generateMarkdown = require('./utils.generateMarkdown');
 
+//List of user prompts to collect information about project to fill out readme file
 const getUserInfo = () => {
     return inquirer.prompt([
         {
@@ -51,27 +51,49 @@ const getUserInfo = () => {
         {
             type: 'list',
             name: 'Licenses:',
-            choices: ['Licenses: MIT License (MIT)', 'IBM Public License 1.0', 'Free Public License']
+            choices: ['Licenses: MIT License (MIT)', 'IBM Public License 1.0', 'APACHE License']
         },
-    ])
-}
-
-
-function makeReadme(fileName, data) {
-    const fileName = fs.writeFileSync("./readme.md")
-    return `# ${data.title}
-  
-    `;
-    }
-getUserInfo()
-    .then(function (data) {
+    ]) .then(function (data) {
         let makeFile = makeReadme(data)
+        const filePath = "readme.md";
+        fs.writeFileSync(filePath, makeFile)
     })
     .then(function () {
         console.log("Success!");
     })
+    .catch(function (error) {
+        console.log(error);
+    })
+}
 
-makeReadme();
+//Function to retrieve the user input using the inquirer package
+getUserInfo()
+
+function makeReadme(data) {
+ 
+//Function that will retrieve and display license badges on the readme file
+let badge;
+    switch (data.license) {
+        case "none":
+            badge = ""
+            break;
+        case "MIT":
+            badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+            break;
+        case "IBM":
+            badge = "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+            break;
+        case "APACHE":
+            badge = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+            break;
+    }
+
+    return `# ${data.type}
+    ## ${data.name}
+    ### ${data.message}
+    # ${data.choices}
+`
   
-module.exports = generateMarkdown;
-  
+}
+
+// module.exports = generateMarkdown;
